dd403b76b51fb165912f6b362a2b221a74d4ad64
ig-aip <Babich-goga@bk.ru>
2026-04-09 02:27:33 +0300

    addedd ip

--- settings_auth.h
+++ settings_auth.h
@@ -2,7 +2,7 @@
 #define SETTINGS_AUTH_H
 #include<string.h>
 
-const std::string IP = "127.0.0.1";
+const std::string IP = "0.0.0.0";
 const int PORT = 55556;
 const size_t SQL_CONNECT_POOL_SIZE = 5;
 #endif // SETTINGS_AUTH_H
